7eb865464d28d99588947b524257678b694f0820
Ngan Trinh <ngantrinhkim367@gmail.com>
2025-08-10 01:51:11 +0700

    cap nhat moi nhat Ngan lam

--- Triox-main/assets/js/script.js
+++ Triox-main/assets/js/script.js
@@ -81,3 +81,97 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+
+// Ngân JS 
+// Mảng dữ liệu khóa học
+  const courses = [
+    {
+      title: "Khóa Học HTML CSS Cơ Bản",
+      img: "assets/images/product-1.png",
+      discount: "-30%",
+      oldPrice: "2.000.000",
+      newPrice: "1.200.000",
+      lessons: 30,
+      exercises: "300 bài",
+      year: 2025,
+      bought: "500+",
+      stock: 10
+    },
+    {
+      title: "Khóa Học JavaScript Nâng Cao",
+      img: "assets/images/product-2.png",
+      discount: "-40%",
+      oldPrice: "3.000.000",
+      newPrice: "1.800.000",
+      lessons: 40,
+      exercises: "450 bài",
+      year: 2025,
+      bought: "800+",
+      stock: 7
+    },
+    {
+      title: "Khóa Học ReactJS",
+      img: "assets/images/product-3.png",
+      discount: "-50%",
+      oldPrice: "4.000.000",
+      newPrice: "2.500.000",
+      lessons: 50,
+      exercises: "500 bài",
+      year: 2025,
+      bought: "1000+",
+      stock: 5
+    },
+  ];
+  const list = document.querySelector(".section-2 .inner-list");
+
+  let html = "";
+  courses.forEach(course => {
+    html += `
+      <div class="product-item">
+        <div class="inner-image">
+          <a href="#">
+            <img src="${course.img}" alt="${course.title}">
+          </a>
+        </div>
+        <div class="inner-discount">
+          <i class="fa-solid fa-bolt"></i> Giảm ${course.discount}
+        </div>
+        <div class="inner-content">
+          <h3 class="inner-title">
+            <a href="#">${course.title}</a>
+          </h3>
+          <div class="inner-prices">
+            <div class="inner-price-old">
+              ${course.oldPrice}<span class="inner-unit">đ</span>
+            </div>
+            <div class="inner-price-new">
+              ${course.newPrice}<span class="inner-unit">đ</span>
+            </div>
+          </div>
+          <div class="inner-desc">
+            <div>Số bài giảng: <b>${course.lessons}</b></div>
+            <div>Số lượng bài tập : <b>${course.exercises}</b></div>
+            <div>Khóa Năm: <b>${course.year}</b></div>
+          </div>
+          <div class="inner-meta">
+            <div class="inner-rating">
+              <div class="inner-number">
+                Số lượt đã mua (${course.bought})
+              </div>
+            </div>
+            <div class="inner-stock">
+              <div class="inner-label">
+                Số chỗ còn:
+              </div>
+              <div class="inner-number">
+                ${course.stock}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+
+  list.innerHTML = html;
